9af46304df2629902d7fc6817bf52e8a092ec527
Susumu Arimoto <susumu2934@gmail.com>
2026-01-17 16:24:22 +0900

    create devise user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_10_114048) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_17_072218) do
   create_table "beverages", charset: "utf8mb4", force: :cascade do |t|
     t.integer "caffeine_mg", default: 0, null: false
     t.datetime "created_at", null: false
@@ -27,6 +27,18 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_10_114048) do
     t.datetime "updated_at", null: false
     t.index ["beverage_id"], name: "index_caffeine_intakes_on_beverage_id"
     t.index ["consumed_at"], name: "index_caffeine_intakes_on_consumed_at"
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "caffeine_intakes", "beverages"
